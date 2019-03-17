107f8a31d624ab89897e71c41fdd9df6034952f8
潘安静 <172244892@qq.com>
2019-03-17 12:30:12 +0800

    Initial Commit

---
+++ ToL/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToL
+//
+//  Created by Jal on 2019/3/17.
+//  Copyright © 2019 anjing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToL/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToL
+//
+//  Created by Jal on 2019/3/17.
+//  Copyright © 2019 anjing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
